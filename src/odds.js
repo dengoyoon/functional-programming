943e5bde462d49fd60a0cc75faea68db89342938
dengoyoon <engus927@naver.com>
2023-01-19 00:56:17 +0900

    odds generator 작성

--- src/odds.js
+++ src/odds.js
@@ -1,5 +1,29 @@
-function* infinity() {}
+/*
+generator 함수를 이용해서 홀수만 뱉는 generator odds를 만들자
+*/
 
-function* limit() {}
+function* infinity(i = 0) {
+  while (true) yield i++;
+}
 
-function* odds() {}
+function* limit(num, iter) {
+  for (const v of iter) {
+    if (v <= num) yield v;
+  }
+}
+
+function* odds(num) {
+  for (const v of limit(num, infinity(1))) {
+    if (v % 2 === 1) yield v;
+  }
+}
+
+const oddsIter = odds(7);
+console.log(oddsIter.next());
+console.log(oddsIter.next());
+console.log(oddsIter.next());
+console.log(oddsIter.next());
+console.log(oddsIter.next());
+console.log(oddsIter.next());
+console.log(oddsIter.next());
+console.log(oddsIter.next());
