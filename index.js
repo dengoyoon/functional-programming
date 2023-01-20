0b1711f99e6da27f8914dc7c312cf199625ed7af
dengoyoon <engus927@naver.com>
2023-01-21 00:37:23 +0900

    fx.js로 map, reduce, filter 이동

---
+++ index.js
@@ -0,0 +1,3 @@
+import { map, reduce, filter } from "./src/fx.js";
+
+console.log(map((a) => a * 2, [1, 2, 3, 4, 5]));
---
+++ src/fx.js
@@ -0,0 +1,28 @@
+export function filter(f, iter) {
+  const res = [];
+  for (const a of iter) {
+    if (f(a)) res.push(a);
+  }
+
+  return res;
+}
+
+export function reduce(f, acc, iter) {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+
+  return acc;
+}
+
+export function map(f, iter) {
+  const res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+}
