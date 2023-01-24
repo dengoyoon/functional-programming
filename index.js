3f299e8375c3ced43da61fb3c6840dd9a011187f
dengoyoon <engus927@naver.com>
2023-01-25 03:09:22 +0900

    pipe로 중복 개선하기

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ const add = (a, b) => a + b;
 
 // go를 사용하여 읽기 좋은 코드로 변경
 // go를 사용하지 않으면 오른쪽에서 왼쪽으로 코드를 읽어야 이해가 되는 코드가 된다.
+
+const base_total_price = (predi) =>
+  pipe(
+    filter(predi),
+    map((a) => a.price),
+    reduce(add)
+  );
+
 go(
   products,
-  filter((p) => p.price < 20000),
-  map((a) => a.price),
-  reduce(add),
+  base_total_price((p) => p.price < 20000),
   log
 );
 
 go(
   products,
-  filter((p) => p.price >= 20000),
-  map((a) => a.price),
-  reduce(add),
+  base_total_price((p) => p.price >= 20000),
   log
 );
