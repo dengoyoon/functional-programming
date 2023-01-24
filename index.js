4a2238ac95d751ce97630dbb97591eb5bba3ff77
dengoyoon <engus927@naver.com>
2023-01-25 04:21:16 +0900

    간단한 카트 페이지 구현 완료

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { log, map, reduce, filter, go, pipe, curry } from "./src/fx.js";
 
 const products = [
-  { name: "반팔티", price: 15000, quantity: 1 },
-  { name: "긴팔티", price: 20000, quantity: 2 },
-  { name: "핸드폰케이스", price: 15000, quantity: 3 },
-  { name: "후드티", price: 30000, quantity: 4 },
-  { name: "바지", price: 25000, quantity: 5 },
+  { name: "반팔티", price: 15000, quantity: 1, is_selected: true },
+  { name: "긴팔티", price: 20000, quantity: 2, is_selected: false },
+  { name: "핸드폰케이스", price: 15000, quantity: 3, is_selected: true },
+  { name: "후드티", price: 30000, quantity: 4, is_selected: false },
+  { name: "바지", price: 25000, quantity: 5, is_selected: false },
 ];
 
 const add = (a, b) => a + b;
@@ -25,27 +25,31 @@ const base_total_price = (predi) => pipe(filter(predi), total_price);
 document.querySelector("#cart").innerHTML = `
   <table>
     <tr>
+      <th></th>
       <th>상품 이름</th>
       <th>가격</th>
       <th>수량</th>
       <th>총 가격</th>
     </tr>
+    ${go(
+      products,
+      sum(
+        (item) => `
+        <tr>
+          <td><input type="checkbox" ${item.is_selected ? "checked" : ""}/></td>
+          <td>${item.name}</td>
+          <td>${item.price}</td>
+          <td><input type="number" value="${item.quantity}"/></td>
+          <td>${item.price * item.quantity}</td>
+        </tr>
+        `
+      )
+    )}
     <tr>
-      <td>반팔티</td>
-      <td>10000</td>
-      <td>3</td>
-      <td>30000</td>
-    </tr>
-    <tr>
-      <td>반팔티</td>
-      <td>10000</td>
-      <td>3</td>
-      <td>30000</td>
-    </tr>
-    <tr>
+      <td></td>
       <td colspan="2">합계</td>
-      <td>6</td>
-      <td>60000</td>
+      <td>${total_quantity(filter((item) => item.is_selected, products))}</td>
+      <td>${total_price(filter((item) => item.is_selected, products))}</td>
     </tr>
   </table>
 `;
