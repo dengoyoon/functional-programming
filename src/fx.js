d6ca0b78234e6e48f115c69b1e3d61392c88b0a6
dengoyoon <engus927@naver.com>
2023-01-27 16:51:35 +0900

    L.flatten, flatten 구현

--- src/fx.js
+++ src/fx.js
@@ -2,7 +2,31 @@ export const log = console.log;
 
 export const L = {};
 
-const takeAll = take(Infinity);
+const isIterable = (a) => a && a[Symbol.iterator];
+
+// 코드를 값으로 다루어 표현력을 높이기 위한 함수 go, pipe, curry
+
+/* 
+go : 값을 평가 하기 위함 (값을 리턴하는 함수)
+pipe : 함수를 합성하기 위함 (함수를 리턴하는 함수, 그냥 함수를 순서대로 실행되게 끔 하는 세트 정도로 이해)
+*/
+
+export const go = (...args) => reduce((a, f) => f(a), args);
+/*
+예시
+go(0, a => a + 1, a => a + 10, a => a + 100, log);
+*/
+
+// pipe는 첫번째 함수의 인자가 여러개를 받아야 할 경우를 생각해서 다음과 같이 작성해야 함.
+export const pipe =
+  (f, ...fs) =>
+  (...as) =>
+    go(f(...as), ...fs);
+/*
+예시
+const f = pipe((a, b) => a + b, a => a + 10, a => a + 100);
+log(f(0));
+*/
 
 // 받아두었던 함수를 나중에 실행시키는 함수.
 // 인자를 하나만 받으면 일단 이후 인자들을 더 받기로 기다리는 상태의 함수가 됨.
@@ -14,9 +38,23 @@ export const curry =
 // const mult = curry((a, b) => a * b);
 // log(mult(3, 4));
 // log(mult(3)(4));
+
 L.filter = curry(function* (f, iter) {
   for (const a of iter) if (f(a)) yield a;
 });
+
+// reduce와 take는 결과를 위한 함수라고 보면 된다.
+// 예를 들어 바로 밑의 join함수는 reduce 계열에 의한 결과 도출함수라고 이해하면 되고
+// find함수의 경우엔 take 계열에 의한 결과 도출 함수라고 이해하면 된다.
+export const take = curry((length, iter) => {
+  const res = [];
+  for (const a of iter) {
+    res.push(a);
+    if (res.length === length) return res;
+  }
+});
+
+const takeAll = take(Infinity);
 
 export const filter = curry(pipe(L.filter, takeAll));
 
@@ -58,17 +96,6 @@ L.range = function* range(length) {
   }
 };
 
-// reduce와 take는 결과를 위한 함수라고 보면 된다.
-// 예를 들어 바로 밑의 join함수는 reduce 계열에 의한 결과 도출함수라고 이해하면 되고
-// find함수의 경우엔 take 계열에 의한 결과 도출 함수라고 이해하면 된다.
-export const take = curry((length, iter) => {
-  const res = [];
-  for (const a of iter) {
-    res.push(a);
-    if (res.length === length) return res;
-  }
-});
-
 // 객체지향적으로 구현된 기존의 Array.prototype.join 보다 훨씬 다형성이 높다는 것을 알 수 있다.
 export const join = curry((separator, iter) =>
   reduce((a, b) => `${a}${separator}${b}`, iter)
@@ -82,26 +109,12 @@ export const find = curry((f, iter) =>
   go(iter, L.filter(f), take(1), ([a]) => a)
 );
 
-// 코드를 값으로 다루어 표현력을 높이기 위한 함수 go, pipe, curry
+L.flatten = function* (iter) {
+  for (const a of iter) {
+    if (isIterable(a)) for (const b of a) yield b;
+    else yield a;
+  }
+};
 
-/* 
-go : 값을 평가 하기 위함 (값을 리턴하는 함수)
-pipe : 함수를 합성하기 위함 (함수를 리턴하는 함수, 그냥 함수를 순서대로 실행되게 끔 하는 세트 정도로 이해)
-*/
-
-export const go = (...args) => reduce((a, f) => f(a), args);
-/*
-예시
-go(0, a => a + 1, a => a + 10, a => a + 100, log);
-*/
-
-// pipe는 첫번째 함수의 인자가 여러개를 받아야 할 경우를 생각해서 다음과 같이 작성해야 함.
-export const pipe =
-  (f, ...fs) =>
-  (...as) =>
-    go(f(...as), ...fs);
-/*
-예시
-const f = pipe((a, b) => a + b, a => a + 10, a => a + 100);
-log(f(0));
-*/
+// 바로 평가되게 함수 구현.
+const flatten = pipe(L.flatten, takeAll);
